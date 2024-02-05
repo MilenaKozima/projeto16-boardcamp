d0de2a14898a5db97289707bdb84685e5ba9d6fc
MilenaKozima <mkozima06@gmail.com>
2024-02-05 15:23:23 -0300

    Criar um jogo

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -5,14 +5,29 @@ export async function getGames(req,res) {
 
         const games = await db.query(`SELECT * FROM games:`);
         res.send(games);
-        
+
     } catch (err) {
         res.status(500).send(err.message)
     }
 }
 
 export async function createGames(req,res) {
+    
+    const { name, image, stockTotal, pricePerDay } = req.body;
+
     try {
+
+        const game = await db.query(`SELECT name FROM games WHERE name = $1;`, [name])
+
+        if (game.rowCount >0)
+        return res.status(409).send({message:'Jogo já existe'});
+
+        await db.query(`
+            INSERT INTO games(name, image, "stockTotal", "pricePerDay")
+                VALUES($1, $2, $3, $4);
+        `, [name, image, stockTotal, pricePerDay]);
+
+        res.sendStatus(201);
 
     } catch (err) {
         res.status(500).send(err.message)
